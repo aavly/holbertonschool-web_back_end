f3384a2419392447f1ff2b94a69bc71ee2361565
aavly <annavyly@gmail.com>
2025-04-16 00:14:24 +1000

    using import and export statements

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import { deflate } from 'zlib';
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+	return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
+
+export default initializeRooms;
