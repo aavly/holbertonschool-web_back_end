1407c9d020b7c513830ac26d6802809085769294
aavly <annavyly@gmail.com>
2025-04-17 15:32:59 +1000

    fixing lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -9,27 +9,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-    get name() {
-      return this._name;
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('length must be a number');
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw TypeError('Students must be an array of strings');
     }
-    get length() {
-      return this._length;
-    }
-    get students() {
-      return this._students;
-    }
-    set name(name) {
-      if (typeof name !== 'string') throw TypeError('name must be a string');
-      this._name = name;
-    }
-    set length(length) {
-      if (typeof length !== 'number') throw TypeError('length must be a number');
-      this._length = length;
-    }
-    set students(students){
-      if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-        throw TypeError('Students must be an array of strings');
-      }
-      this._students = students;
+    this._students = students;
   }
 }
