37b52099a25bc38e6fcee1d4bc4e743731984acb
aavly <annavyly@gmail.com>
2025-04-17 15:30:18 +1000

    fixing lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -8,7 +8,7 @@ export default class HolbertonCourse {
     this._name = name;
     this._length = length;
     this._students = students;
-    }
+  }
     get name() {
       return this._name;
     }
