e78f89cebb1f50fec8c32d83565e35802c95113a
aavly <annavyly@gmail.com>
2025-04-20 23:02:10 +1000

    forcing override oof method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    this._sqft = newSqft;
-  }
-
-  evacuationWarningMessage() {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
