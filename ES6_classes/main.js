df66062cac54112b81898394182eb999c6c36c06
aavly <annavyly@gmail.com>
2025-04-20 23:30:24 +1000

    toString() method

--- ES6_classes/main.js
+++ ES6_classes/main.js
@@ -1,8 +1,5 @@
-import Building from './5-building.js';
+import Airport from "./7-airport.js";
 
-import SkyHighBuilding from './6-sky_high.js';
-
-const building = new SkyHighBuilding(140, 60);
-console.log(building.sqft);
-console.log(building.floors);
-console.log(building.evacuationWarningMessage());
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
