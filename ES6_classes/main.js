64d6502746f1196d0cf8cbd5ce36fd57f6aec5c6
aavly <annavyly@gmail.com>
2025-04-20 23:14:53 +1000

    inheritance

--- ES6_classes/main.js
+++ ES6_classes/main.js
@@ -1,13 +1,8 @@
 import Building from './5-building.js';
 
-const b = new Building(100);
-console.log(b);
+import SkyHighBuilding from './6-sky_high.js';
 
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
