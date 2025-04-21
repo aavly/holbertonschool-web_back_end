a5b408714651d93c19119b48a0e83713d482b3c6
aavly <annavyly@gmail.com>
2025-04-21 13:12:45 +1000

    matching student id from two arrays

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,14 @@
 /* eslint-disable */
 
-const updateStudentGradeByCity = (studentsList, city, newGrades="N/A") => {
+const updateStudentGradeByCity = (studentsList, city, newGrades=[]) => {
 	return studentsList
-		.filter(student => student.location === city)
-		.map(student => ({
-			...student,
-			grade: newGrades,
-		}));
+		.filter((student) => student.location === city)
+		.map((student) => {
+			const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+			return {
+			  ...student,
+			  grade: gradeObject ? gradeObject.grade : "N/A",
+			};
+		  });
 };
-
 export default updateStudentGradeByCity;
