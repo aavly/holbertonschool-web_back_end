a6d3581604111b1c08c344cf4585b0c95db58ce8
aavly <annavyly@gmail.com>
2025-04-23 15:19:24 +1000

    return a promise with with resolve() and reject()

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+const getFullResponseFromAPI = (success) => {
+	return new Promise((resolve, reject) => {
+		if (success) {
+			const object = { status: 200, message: 'Success' };
+			resolve(object);
+		} else {
+			reject("The fake API is not working currently");
+		}
+	});
+}
+
+
+export default getFullResponseFromAPI;
