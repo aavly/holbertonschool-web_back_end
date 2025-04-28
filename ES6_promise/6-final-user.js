e0937e15159038426252849d2a96c7bbe6519b77
aavly <annavyly@gmail.com>
2025-04-28 22:14:49 +1000

    .then() to map results for rejected promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,6 +6,21 @@ import uploadPhoto from './5-photo-reject.js';
 export default function handleProfileSignup(firstName, lastName, fileName) {
 	return Promise.allSettled([
 		signUpUser(firstName, lastName),
-		uploadPhoto(fileName)
-	]);
-}
+		uploadPhoto(fileName),
+	]).then(
+		(results) => results.map(
+			(result) => {
+				const newResult = {
+			  		status: result.status,
+				};
+	
+		if (result.status === 'rejected') {
+			newResult.value = result.reason.toString();
+		} else {
+			newResult.value = result.value;
+		}
+			return newResult;
+		  },
+		),
+	  );
+	}
