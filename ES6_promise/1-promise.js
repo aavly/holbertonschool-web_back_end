e9dd0c7c545174c3c8c567d7d703c2c6ced7faa7
aavly <annavyly@gmail.com>
2025-04-28 13:43:44 +1000

    changed attribute name from message to body

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,7 +3,7 @@
 const getFullResponseFromAPI = (success) => {
 	return new Promise((resolve, reject) => {
 		if (success) {
-			const object = { status: 200, message: 'Success' };
+			const object = { status: 200, body: 'Success' };
 			resolve(object);
 		} else {
 			reject("The fake API is not working currently");
