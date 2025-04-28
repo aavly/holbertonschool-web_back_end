26164846f49a50220c6abae18097423bea6e46f3
aavly <annavyly@gmail.com>
2025-04-28 21:31:28 +1000

    simple promise using Promise.resolve({})

--- ES6_promise/main.js
+++ ES6_promise/main.js
@@ -1,4 +1,3 @@
-import handleResponseFromAPI from "./2-then";
+import signUpUser from "./4-user-promise";
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+console.log(signUpUser("Bob", "Dylan"));
