a5872c76ee1065b0d88e9dbf5d6dbe59699d6fcc
aavly <annavyly@gmail.com>
2025-04-29 16:20:43 +1000

    including type annotation and docstring

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,12 @@
 import asyncio
 import random
 
-async def wait_random(max_delay=10):
+
+async def wait_random(max_delay=10) -> float:
+    """
+    coroutine that waits for the amount of time randomly generated
+    then returns that same number once the time has passed
+    """
     wait_length = random.uniform(0, max_delay)
     await asyncio.sleep(wait_length)
     return wait_length
