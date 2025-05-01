3ee5b0cb12602c1a1c9be4091131f49a45757f70
aavly <annavyly@gmail.com>
2025-05-01 12:59:35 +1000

    using *args

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Task 2. executes async_comprehension 4 times 
+Task 2. executes async_comprehension 4 times
 in parallel using async.gather. returns the total runtime
 """
 
@@ -16,12 +16,8 @@ async def measure_runtime() -> float:
     in parallel using async.gather. returns the total runtime
     """
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    coroutines = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*coroutines)
     end = time.perf_counter()
     total_time = end - start
     return total_time
