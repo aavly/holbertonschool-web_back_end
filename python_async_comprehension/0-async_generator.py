045adcd2ecd7f3625393c4c12590ad30c68efe9f
aavly <annavyly@gmail.com>
2025-05-01 12:23:33 +1000

    added variable annotations

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,12 @@ Task 0. using async_generator to yield values
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-
-async def async_generator():
-    """Task 0. using async_generator to yield values """
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Task 0. using async_generator to yield values
+    """
     random_number = random.uniform(0, 10)
     for i in range(10):
         await asyncio.sleep(1)
