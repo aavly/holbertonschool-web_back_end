a06fe57e17863e2418540afdd2ff7822b18d106f
aavly <annavyly@gmail.com>
2025-05-05 16:23:24 +1000

    mixed lists require Union for annotations

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,11 +2,14 @@
 """
 Task 6.
 """
-from typing import List
+from typing import Union, List
 
 
-def sum_list(input_list: List[float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    float sum of mixed list of ints and floats
+    """
     sum = 0
-    for i in input_list:
+    for i in mxd_lst:
         sum += i
     return sum
