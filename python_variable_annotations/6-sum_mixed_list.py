61bbaf04f726cbf15a3b06abc3405f144e3ba264
aavly <annavyly@gmail.com>
2025-05-05 16:24:54 +1000

    checker isn't a fan of how i put float before int

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Task 6.
 from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     float sum of mixed list of ints and floats
     """
