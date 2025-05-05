d547d2728d0907c02b3f95170a8ea29cc41488b0
aavly <annavyly@gmail.com>
2025-05-05 23:32:21 +1000

    hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -48,7 +48,7 @@ class Server:
 
         return dataset[start:end] if start < len(dataset) else []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         data = self.get_page(page, page_size)
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
