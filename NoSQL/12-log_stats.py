3c1c914df6d60beeb19d2515ef86476f4adb842e
aavly <annavyly@gmail.com>
2025-05-12 19:47:43 +1000

    running the file as a script

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,20 +5,26 @@
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
 
-nginx_collection = client.logs.nginx
+def log_stats():
+    client = MongoClient('mongodb://127.0.0.1:27017')
 
-amount_of_logs = nginx_collection.count_documents({})
+    nginx_collection = client.logs.nginx
 
-print(f"{amount_of_logs} logs")
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = nginx_collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    amount_of_logs = nginx_collection.count_documents({})
 
-status_check_count = nginx_collection.count_documents({
-    "method": "GET",
-    "path": "/status"
-})
-print(f"{status_check_count} status check")
+    print(f"{amount_of_logs} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    amount_status_check = nginx_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(f"{amount_status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
